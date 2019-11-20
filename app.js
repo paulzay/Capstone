5f2f74140d0ea9d7a0eaa8908b1b6525174271bb
paul zay <paulotieno2@gmail.com>
2019-11-20 12:06:06 +0300

    catered for CORS errors

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) =>{
-  res.json({message: "test succeeded"})
-})
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST ,PUT ,DELETE, PATCH, OPTIONS');
+  next();
+});
 
-module.exports = app
+
+module.exports = app;
