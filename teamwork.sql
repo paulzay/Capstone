9eb69bf79f17b805aaf8f13bdfea7868d2bfd21b
paul zay <paulotieno2@gmail.com>
2019-11-20 16:21:04 +0300

    added a querying file

---
+++ teamwork.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Employee (
+  employee_id SERIAL PRIMARY KEY,
+  firstName VARCHAR(50) NOT NULL,
+  lastName VARCHAR(50) NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  gender VARCHAR(50) NOT NULL,
+  jobRole VARCHAR(50) NOT NULL,
+  department VARCHAR(50) NOT NULL,
+  address VARCHAR(50) NOT NULL,
+  is_admin BOOLEAN NOT NULL
+);
+
+
