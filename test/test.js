7d2149424739cce0464c93e5d807e82253b05939
paul zay <paulotieno2@gmail.com>
2019-11-23 15:28:31 +0300

    write the root path test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const assert = require('assert');
+const baseurl = "http://localhost:3000/";
+const app = require('../app');
+
+describe('shows homepage test', () => {
+  describe('GET /', () => {
+    it('returns status code 200', () => {
+
+    });
+  });
+});
+
